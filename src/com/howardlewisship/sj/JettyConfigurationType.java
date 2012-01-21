e8558d786fe5074430acc890962b6014f9d0d2b6
Howard M. Lewis Ship <hlship@gmail.com>
2012-01-20 21:33:32 -0800

    Provide a useless ConfigurationFactory as a placeholder

--- src/com/howardlewisship/sj/JettyConfigurationType.java
+++ src/com/howardlewisship/sj/JettyConfigurationType.java
@@ -1,6 +1,10 @@
 package com.howardlewisship.sj;
 
+import com.intellij.execution.configuration.ConfigurationFactoryEx;
 import com.intellij.execution.configurations.ConfigurationTypeBase;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 /**
  * Hooks into the configurations menu.
@@ -14,5 +18,15 @@ public class JettyConfigurationType extends ConfigurationTypeBase {
           "Slick Jetty Launch",
           "Launches the web application directly from the workspace.",
           null);
+
+    addFactory(new ConfigurationFactoryEx(this) {
+      @Override
+      public RunConfiguration createTemplateConfiguration(Project project) {
+
+        Messages.showMessageDialog("Request for template configuration.", "Slick Jetty", null);
+
+        return null;
+      }
+    });
   }
 }
